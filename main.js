d30b882ad80d9b43493542e130c298f805ad6348
Komol Parpixodjayev <www.komol8689@gmail.com>
2025-04-10 09:20:12 +0500

    New file

---
+++ main.js
@@ -0,0 +1,20 @@
+let person={
+    name:"Usmon",
+    age:27,
+    gender:"Male",
+    hobbies:["cs","fifa","ps"],
+    skills:["sleep","runner","swimmer"]
+}
+// console.log(person)
+// alert(person.name)
+
+const firstName=prompt("Ismingizni kiriting?")
+const lastName=prompt("Familyangizni kiriting?")
+const age=parseInt(prompt("Yoshingizni kiriting?"))
+
+console.log(firstName)
+console.log(lastName)
+console.log(typeof age)
+console.log(age)
+
+
